d374daa1c3d3488fb3ccd2c218b4bb27a46b37df
Musa Masroor <musamasroor786@gmail.com>
2024-03-06 04:25:34 +0500

    edited a file

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,26 +1,18 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 
 const NewsItem = (props) => {
-  const { title, description, imageUrl, newsUrl, author, date, source } = props;
-  const cardRef = useRef(null);
-
-  useEffect(() => {
-    // Get the height of the tallest card
-    const tallestCardHeight = Math.max(
-      ...Array.from(document.querySelectorAll(".card")).map(
-        (card) => card.offsetHeight
-      )
-    );
-    // Set the height of all cards to match the tallest card
-    Array.from(document.querySelectorAll(".card")).forEach(
-      (card) => (card.style.height = tallestCardHeight + "px")
-    );
-  }, []);
-
+  let { title, description, imageUrl, newsUrl, author, date, source } = props;
   return (
     <div className="my-3">
-      <div className="card" ref={cardRef}>
-        <div style={{ justifyContent: "flex-end" }} className="d-flex">
+      <div className="card">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            position: "absolute",
+            right: "0",
+          }}
+        >
           <span className="badge rounded-pill bg-danger"> {source} </span>
         </div>
         <img
@@ -33,7 +25,7 @@ const NewsItem = (props) => {
           alt="..."
         />
         <div className="card-body">
-          <h5 className="card-title">{title}</h5>
+          <h5 className="card-title">{title} </h5>
           <p className="card-text">{description}</p>
           <p className="card-text">
             <small className="text-muted">
